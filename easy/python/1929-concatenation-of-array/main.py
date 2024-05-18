8492c186b321d75867881c28f5cd1c3b137d2606
wendelfrota <wendelalves898@gmail.com>
2024-05-17 23:06:20 -0300

    add: 1929. concatenation of array

---
+++ easy/python/1929-concatenation-of-array/main.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        return nums + nums
+
+
+# print(Solution().getConcatenation([1, 2, 1]))
+# print(Solution().getConcatenation([1, 3, 2, 1]))
--- easy/python/2879-display-the-first-three-rows/main.py
+++ easy/python/2879-display-the-first-three-rows/main.py
@@ -3,3 +3,12 @@ import pandas as pd
 
 def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
     return employees.head(3)
+
+
+# df = pd.DataFrame({
+#     'Nome': ['Ana', 'Bruno', 'Carlos', 'Diana'],
+#     'Idade': [23, 34, 45, 29],
+#     'Cidade': ['São Paulo', 'Rio de Janeiro', 'Belo Horizonte', 'Curitiba']
+# })
+#
+# print(selectFirstRows(df))
