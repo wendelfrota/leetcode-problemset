c13ae136b2e2e8f83f22540f24a6a8efa079016f
wendelfrota <wendelalves898@gmail.com>
2024-05-19 08:16:38 -0300

    add: py/2840. check if strings can be made equal

---
+++ medium/python/2840-check-if-strings-can-be-made-equal/main.py
@@ -0,0 +1,7 @@
+class Solution:
+    def checkStrings(self, s1: str, s2: str) -> bool:
+        return (sorted(s1[::2]) == sorted(s2[::2])) and (sorted(s1[1::2]) == sorted(s2[1::2]))
+
+
+# print(Solution().checkStrings('abcdba', 'cabdab'))
+# print(Solution().checkStrings('abe', 'bea'))
