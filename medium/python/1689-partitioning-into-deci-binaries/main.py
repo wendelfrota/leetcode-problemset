b08edfa205c8e6380c87d2fd76e619032067c77a
wendelfrota <wendelalves898@gmail.com>
2024-05-21 20:03:02 -0300

    add: py/1689. partitioning into deci-binaries

---
+++ medium/python/1689-partitioning-into-deci-binaries/main.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minPartitions(self, n: str) -> int:
+        return int(max(n))
+
+
+# print(Solution().minPartitions('32'))
+# print(Solution().minPartitions('82734'))
+# print(Solution().minPartitions('27346209830709182346'))
