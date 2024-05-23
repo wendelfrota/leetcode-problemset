9c8f8190c8ffd8a8820e0ebe0dcf94928b76724f
wendelfrota <wendelalves898@gmail.com>
2024-05-22 21:46:33 -0300

    add: py/2469. convert the temperature

---
+++ easy/python/2469-convert-the-temperature/main.py
@@ -0,0 +1,7 @@
+class Solution:
+    def convertTemperature(self, celsius: float) -> list[float]:
+        return [celsius + 273.15, celsius * 1.80 + 32.00]
+
+
+# print(Solution().convertTemperature(36.50))
+# print(Solution().convertTemperature(122.11))
