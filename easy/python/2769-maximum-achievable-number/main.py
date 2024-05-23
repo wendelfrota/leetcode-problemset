cfe65f1bf066ef6a931050f42f1e7a563af36446
wendelfrota <wendelalves898@gmail.com>
2024-05-22 23:42:44 -0300

    add: py/2769. maximum achievable number

---
+++ easy/python/2769-maximum-achievable-number/main.py
@@ -0,0 +1,7 @@
+class Solution:
+    def theMaximumAchievableX(self, num: int, t: int) -> int:
+        return num + (t * 2)
+
+
+# print(Solution().theMaximumAchievableX(4, 1))
+# print(Solution().theMaximumAchievableX(3, 2))
